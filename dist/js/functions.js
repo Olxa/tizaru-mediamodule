bdfce5d525d19fc8e88c9baa69dded13f14cdfd2
Oli4 <olxaweb@mail.ru>
2021-09-07 21:21:48 +0300

    билд

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -552,6 +552,18 @@ document.addEventListener("DOMContentLoaded", function () {
         return false;
     });
 
+    //Плавный скролл для якорей
+    $(document).ready(function () {
+        $('a[href*=#]').bind("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+        return false;
+    });
+
 
     //Mobil filter
 
